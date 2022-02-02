07b47b89d79c5d847461c72a1501498fc82700c7
saya0118 <chemi0802@gmail.com>
2022-02-02 13:08:48 -0800

    deleted word_changed from actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,3 @@
-export const WORD_CHANGED = 'WORD_CHANGED'
-export const Input = () => {
-    return {
-        type: 'WORD_CHANGED',
-        payload: ''
-    }
-}
-
 export const LIST_ADDED = 'LIST_ADDED'
 export const Add = () => {
     return {
